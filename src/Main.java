3fd67119214b737a2596bd2abed8c7f775c79bc9
heinriej <heinirej@mail.uc.edu>
2024-09-19 14:56:43 -0400

    complete

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main
 {
     public static void main(String[] args)
     {
+        double purchasePrice = 23.99;
+        double salesTax = 0;
+        final double TAX_RATE = 0.05;
 
+        salesTax = purchasePrice * TAX_RATE;
+
+        System.out.println("The sales tax on a purchase of " + purchasePrice + " is " + salesTax);
     }
 }
